e6afc84456e1f4302d0f7f1dd4f032ccaae85502
horizoncoders <dbelimenko.99@gmail.com>
2023-02-08 16:35:17 +0200

    added sort param

--- src/api/repositoryApi.ts
+++ src/api/repositoryApi.ts
@@ -1,7 +1,7 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 
 export const getRepositories = createAsyncThunk('user/repositories', async function(username:string){
-    const response = await fetch (`${process.env.REACT_APP_API_PATH}/users/${username}/repos`)
+    const response = await fetch (`${process.env.REACT_APP_API_PATH}/users/${username}/repos?sort=updated`)
     return await response.json()
 })
 
